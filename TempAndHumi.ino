5e7b36b87f9c850d3f0389e09200175b036ffdc1
piry9200 <ymt104ro09@gmail.com>
2022-06-11 10:28:33 +0900

    UDP通信の機能を追加する準備

--- TempAndHumi.ino
+++ TempAndHumi.ino
@@ -1,23 +1,57 @@
-#include<DHTesp.h>
-#define sensorPin 32
+#include <WiFi.h>
+#include <WiFiUDP.h>
+
+const char ssid[] = "************"; // ルーターのSSID
+const char pass[] = "************";  // ルーターのpassword
+
+static WiFiUDP wifiUdp; 
+static const char *kRemoteIpadr = "***.***.***.**"; //送信先のIPアドレス
+static const int kRmoteUdpPort = 9000; //送信先のポート
+
+static const int sensorPin = 32;
 DHTesp dht;
 
-void setup() {
+static void WiFi_setup()
+{
+  static const int kLocalPort = 7000;  //自身のポート
+
+  WiFi.begin(ssid, pass);
+  while( WiFi.status() != WL_CONNECTED) {
+    delay(500);  
+  }  
+  wifiUdp.begin(kLocalPort);
+}
+
+static void Serial_setup()
+{
   Serial.begin(115200);
+  Serial.println(""); //　改行用
+}
+
+void setup() {
+  Serial_setup();
+  WiFi_setup();
   dht.setup(sensorPin,DHTesp::DHT11);
 }
 
-void loop() {
-  TempAndHumidity newValues = dht.getTempAndHumidity();
-  if(dht.getStatus() != 0){
+void loop() 
+{
+  if (wifiUdp.parsePacket()) { //パケットを受信したら実行する
+
+    TempAndHumidity newValues = dht.getTempAndHumidity();
+    if(dht.getStatus() != 0){
     Serial.println("DHT11 error status:"+String(dht.getStatusString()));
+    }
+    float temp = newValues.temperature;
+    float hemi = newValues.humidity;
+
+    wifiUdp.beginPacket(kRemoteIpadr, kRemoteUdpPort);
+    wifiUdp.write(temp);
+    wifiUdp.write(hemi);
+    wifiUdp.endPacket();  
+
+    delay(3000);
   }
-  float temp = newValues.temperature;
-  float hemi = newValues.humidity;
-  Serial.print("温度:");
-  Serial.print(temp);
-  Serial.print(" ");
-  Serial.print("湿度:");
-  Serial.println(hemi);
-  delay(3000);
+
 }
+
